8fc3c2a51df06ead1ce56f3eb1b6ebe5bcaff850
Bahram Zamani <bahram.zamani@dazn.com>
2023-02-22 14:31:55 +0000

    Bahram Zamani

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const port = 3000;
 
-require('./routes/card.routes')(app);
+require("./routes/card.routes")(app);
 
-app.get('/', (req, res) => {
-  res.send('Hello cards!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello cards!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- test/card.test.js
+++ test/card.test.js
@@ -2,10 +2,54 @@ const request = require("supertest")("http://localhost:3000");
 const expect = require("chai").expect;
 
 describe("GET /cards", function () {
-  it("returns all cards", async function () {
-    const response = await request.get("/api/cards");
+  // it.skip("returns all cards", async function () {
+  //   const response = await request.get("/api/cards");
+  //   console.log({ response });
 
-    expect(response.status).to.eql(200);
-    expect(response.body.length).to.be.greaterThan(0);
+  //   expect(response.status).to.eql(200);
+  //   expect(response.body.length).to.be.greaterThan(0);
+  // });
+
+  it("add new card", async function () {
+    const request = require("supertest")("http://localhost:3000");
+    let response = await request
+      .post("/api/card")
+      .set("Content-type", "application/json")
+      .send({
+        name: "BZ",
+        pan: "79927398713",
+        limit: "23435",
+        balance: 10,
+      })
+      .expect(201);
+    console.log({ resp: response.body });
+
+    expect(response.status).to.eql(201);
+
+    response = await request
+      .put("/api/card")
+      .set("Content-type", "application/json")
+      .send({
+        id: "GkM4e2CY4",
+        name: "BZU",
+        pan: "79927398713",
+        limit: "23435",
+        balance: 10,
+      })
+      .expect(202);
+
+    console.log({ resp: response.body });
+
+    expect(response.status).to.eql(202);
+
+    response = await request
+      .delete("/api/card/GkM4e2CY4")
+      .expect(202);
+
+    console.log({ resp: response.body });
+
+    expect(response.status).to.eql(202);
+
+
   });
 });
